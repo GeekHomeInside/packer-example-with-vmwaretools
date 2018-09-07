f2bd07a86e41dcfa1c23a556d2106b601d7926ee
Adrien DEVIENNE <34829865+guiadco@users.noreply.github.com>
2018-09-07 16:03:41 +0200

    Update Vmware Tools & System Config

--- scripts/vmware-tools.sh
+++ scripts/vmware-tools.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-yum install -y perl net-tools
+echo "Install dependencies"
+yum install -y perl net-tools gcc fuse-libs bzip2 gcc-c++ kernel-devel-`uname -r` make perl
 
 # Mount VMware Tools ISO file
+echo "Mount VMware Tools ISO file"
 mount -t iso9660 -o loop /root/linux.iso /mnt
 
 # Execute the installer
+echo "Execute the installer"
 cd /tmp
 cp /mnt/VMwareTools-*.gz .
 tar zxvf VMwareTools-*.gz
 ./vmware-tools-distrib/vmware-install.pl -d
 
 # Unmount ISO file
+echo "Unmount ISO file"
 umount /mnt
 
 # Delete ISO file
+echo "Delete ISO file"
 rm -f /root/linux.iso
 
 # Delete copied files from ISO
+echo "Delete copied files from ISO"
 rm -rf VMwareTools-.gz vmware-tools-distrib
